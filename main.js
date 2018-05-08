acfbd0ff5c5247d3180c677fae0a1a886a280a0a
Benjie <benjiecodes@gmail.com>
2018-05-08 14:53:34 -0400

    needs to be run on a server

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 
 recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", e => {
+  console.log(e);
+});
+
+recognition.start();
